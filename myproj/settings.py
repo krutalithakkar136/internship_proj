8e6e5bf08e87f641bd39f482db8ccbebf1cdfcec
krutali <krutalithakkar136@gmail.com>
2023-08-02 15:26:41 +0530

    [MOD] static files changes

--- myproj/settings.py
+++ myproj/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-
-
+STATIC_URL = 'static/'
+# STATIC_DIR = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -162,7 +162,7 @@ APPEND_SLASH=False
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR / 'templates'
-STATIC_DIR = BASE_DIR / 'static'
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
